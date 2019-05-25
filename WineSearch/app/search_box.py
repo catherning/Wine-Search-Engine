4b4541f8c127be8f46bad3d5431404b6981da9e1
catherning <39702739+catherning@users.noreply.github.com>
2019-05-25 17:25:02 +0800

    Going back to prev commit
    Can do two consecutive queries

--- WineSearch/app/search_box.py
+++ WineSearch/app/search_box.py
@@ -3,7 +3,5 @@ from wtforms import StringField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired 
 
 class SearchBox(FlaskForm):
-    query = StringField('Describe the wine of your dreams...')#, validators=[DataRequired()])
-    submit = SubmitField('I wanna get drunk with good wines!') #To change
-    # score_choices = [('Score','Above 50'),('Score','Above 75'),('Score','Above 90')]
-    # select = SelectField('Search for music:', choices=score_choices)
+    query = StringField('WineSearch')#, validators=[DataRequired()])
+    submit = SubmitField('Search')
